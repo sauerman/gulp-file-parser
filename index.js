83390f9b1fffa2bc7e7ebb679f31a08b05a55340
gyson <eilian.yunsong@gmail.com>
2014-07-04 22:59:49 +0800

    add test

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 
-module.exports = function (parseFunc, options) {
+function isPromise (obj) {
+    return obj.then && (typeof obj.then === 'function');
+}
+
+function isThunk (obj) {
+    return typeof obj === 'function';
+}
+
+module.exports = function (options) {
     options = options || {};
 
-    const PLUGIN_NAME = options.name || "no name";
-    const EXTENSION = options.extension;
+    var PLUGIN_NAME = options.name || "no name";
+    var EXTENSION = options.extension;
+
+    var parseFunc = options.function || options.func || (function (x) { return x; });
 
     return function () {
     	// options to parse function
     	var opts = Array.prototype.slice.call(arguments, 0);
 
 	    return through.obj(function (file, enc, callback) {
+
 	        if (file.isNull()) {
 	            this.push(file);
 	            return callback();
@@ -29,19 +40,38 @@ module.exports = function (parseFunc, options) {
 	            try {
 	            	var args = [file.contents.toString()];
 	            	opts.forEach(function (opt) { args.push(opt); });
-	                file.contents = new Buffer(parseFunc.apply(null, args));
-	                if (EXTENSION) {
-	                	file.path = gutil.replaceExtension(file.path, EXTENSION);
-	                }
-	                this.push(file);
+
+                    var result = parseFunc.apply(null, args);
+
+                    if (isPromise(result)) {
+                        return result.then(resolve, reject);
+                    }
+
+                    if (isThunk(result)) {
+                        return result(function (err, data) {
+                            err ? reject(err) : resolve(data);
+                        });
+                    }
+
+                    // return string data
+                    resolve(result)
+
 	            } catch (error) {
-	                this.emit('error', new PluginError(PLUGIN_NAME, error));
+                    reject(new PluginError(PLUGIN_NAME, error));
 	            }
-	            return callback();
 	        }
+
+            function resolve (data) {
+                file.contents = new Buffer(data);
+                if (EXTENSION) {
+                    file.path = gutil.replaceExtension(file.path, EXTENSION);
+                }
+                callback(null, file);
+            }
+
+            function reject (err) {
+                callback(err);
+            }
 	    });
 	};
 }
-
-// usage:
-//module.exports = require('gulp-file-parser')(, { name: "gulp-jeml", extension: ".js" })
--- test/test.js
+++ test/test.js
@@ -1,7 +1,109 @@
 
+require('mocha');
+
 // add test here
-var parser = require('../index');
+var fs = require('fs');
+var gulp = require('gulp');
+var should = require('should');
+var parser = require('gulp-file-parser');
+var Promise = require('es6-promise').Promise;
 
-// TODO...
+describe('gulp-file-parser', function () {
 
+    var filepath = __dirname + '/test'
+    var file_content = 'hello, world';
+    var file_extra = "!"
 
+    before('init file', function () {
+        fs.writeFileSync(filepath + '.txt', file_content, 'utf8');
+    });
+
+    it('should be able to parse string', function (done) {
+        function parse (data) {
+            return data + file_extra;
+        }
+        gulp.src(filepath + '.txt')
+            .pipe(parser({
+                name: 'test',
+                extension: '.str',
+                func: parse
+            })())
+            .pipe(gulp.dest(__dirname))
+            .on('end', function () {
+                fs.readFileSync(filepath + '.str', 'utf8')
+                    .should.equal(file_content + file_extra);
+                done();
+            });
+    });
+
+    it('should be able to parse thunk', function (done) {
+        function parse (data) {
+            return function (cb) {
+                cb(null, data + file_extra);
+            };
+        }
+        gulp.src(filepath + '.txt')
+            .pipe(parser({
+                name: 'test',
+                extension: '.thunk',
+                func: parse
+            })())
+            .pipe(gulp.dest(__dirname))
+            .on('end', function () {
+                fs.readFileSync(filepath + '.thunk', 'utf8')
+                    .should.equal(file_content + file_extra);
+                done();
+            });
+    });
+
+    it('shoule be able to parse promise', function (done) {
+        function parse (data) {
+            return new Promise(function (resolve, reject) {
+                resolve(data + file_extra);
+            });
+        }
+        gulp.src(filepath + '.txt')
+            .pipe(parser({
+                name: 'test',
+                extension: '.promise',
+                func: parse
+            })())
+            .pipe(gulp.dest(__dirname))
+            .on('end', function () {
+                fs.readFileSync(filepath + '.promise', 'utf8')
+                    .should.equal(file_content + file_extra);
+                done();
+            });
+    });
+
+    it('should be able to handle extra arguments', function (done) {
+        function parse (data, a_0, a_1, a_2) {
+            a_0.should.equal(0);
+            a_1.should.equal(1);
+            a_2.should.equal(2);
+            arguments.length.should.equal(4);
+            return data + file_extra;
+        }
+        gulp.src(filepath + '.txt')
+            .pipe(parser({
+                name: 'test',
+                extension: '.str',
+                func: parse
+            })(0, 1, 2))
+            .pipe(gulp.dest(__dirname))
+            .on('end', function () {
+                fs.readFileSync(filepath + '.str', 'utf8')
+                    .should.equal(file_content + file_extra);
+                done();
+            });
+    });
+
+    after('clear file', function () {
+        // clear file
+        fs.unlinkSync(filepath + '.txt');
+        fs.unlinkSync(filepath + '.str');
+        fs.unlinkSync(filepath + '.thunk');
+        fs.unlinkSync(filepath + '.promise');
+    });
+
+});
